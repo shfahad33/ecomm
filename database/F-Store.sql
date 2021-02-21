5f57caf0fa0e0ad713b09240fafb80a6743eb44a
fahad-sheikh <uetstudent60@gmail.com>
2021-02-21 15:46:10 +0500

    first commit

---
+++ database/F-Store.sql
@@ -0,0 +1,270 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2016 at 12:09 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `F-Store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `Category_ID` int(255) NOT NULL,
+  `Category_Name` varchar(123) NOT NULL,
+  `Discription` varchar(255) NOT NULL,
+  `Picture` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+
+
+-- --------------------------------------------------------
+
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `contact_id` int(11) NOT NULL,
+  `Name` varchar(25) NOT NULL,
+  `Email` varchar(29) NOT NULL,
+  `Phone` varchar(29) NOT NULL,
+  `Subject` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `Cust_Id` int(15) NOT NULL,
+  `FullName` varchar(25) NOT NULL DEFAULT '',
+  `UserName` varchar(255) NOT NULL DEFAULT '',
+  `Phone` varchar(25) NOT NULL DEFAULT '',
+  `Email` varchar(55) NOT NULL DEFAULT '',
+  `Password` varchar(20) NOT NULL DEFAULT '',
+  `Re_Password` varchar(20) NOT NULL DEFAULT '',
+  `Country` varchar(25) NOT NULL DEFAULT '',
+  `City` varchar(25) NOT NULL DEFAULT '',
+  `Adress` varchar(55) NOT NULL DEFAULT '',
+  `PostalCode` varchar(25) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `Employee_ID` int(95) NOT NULL,
+  `Employee_Name` varchar(25) NOT NULL,
+  `Username` varchar(25) NOT NULL,
+  `Password` varchar(25) NOT NULL,
+  `Picture` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `order_ID` int(255) NOT NULL,
+  `Full_Name` varchar(25) NOT NULL,
+  `Email` varchar(55) NOT NULL,
+  `Postal_Code` varchar(55) NOT NULL,
+  `Address` varchar(55) NOT NULL,
+  `Country` varchar(55) NOT NULL,
+  `City` varchar(55) NOT NULL,
+  `Phone` varchar(55) NOT NULL,
+  `Warehouse_ID` int(255) NOT NULL,
+  `Dilivery_Address` varchar(75) NOT NULL,
+  `Total_Amount` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `Product_ID` int(255) NOT NULL,
+  `productName` varchar(77) NOT NULL,
+  `Category_ID` int(255) NOT NULL,
+  `Model` varchar(50) NOT NULL,
+  `Type` varchar(50) NOT NULL,
+  `Warehouse_ID` int(255) NOT NULL,
+  `Description` varchar(500) NOT NULL,
+  `Price` varchar(255) NOT NULL,
+  `Picture` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warehouse`
+--
+
+CREATE TABLE `warehouse` (
+  `Warehouse_ID` int(255) NOT NULL,
+  `Country` varchar(25) NOT NULL,
+  `City` varchar(25) NOT NULL,
+  `Address` varchar(55) NOT NULL,
+  `PostalCode` varchar(55) NOT NULL,
+  `Email` varchar(35) NOT NULL,
+  `Warehouse` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`Category_ID`);
+
+--
+
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`contact_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`Cust_Id`),
+  ADD UNIQUE KEY `UserName` (`UserName`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`Employee_ID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `Warehouse_ID` (`Warehouse_ID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`Product_ID`),
+  ADD KEY `Category_ID` (`Category_ID`),
+  ADD KEY `Warehouse_ID` (`Warehouse_ID`);
+
+
+
+--
+-- Indexes for table `warehouse`
+--
+ALTER TABLE `warehouse`
+  ADD PRIMARY KEY (`Warehouse_ID`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD UNIQUE KEY `PostalCode` (`PostalCode`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `Category_ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+--
+
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `Cust_Id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `Employee_ID` int(95) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `order_ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `Product_ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `tblsongs`
+--
+ALTER TABLE `tblsongs`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+--
+-- AUTO_INCREMENT for table `warehouse`
+--
+ALTER TABLE `warehouse`
+  MODIFY `Warehouse_ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`Warehouse_ID`) REFERENCES `warehouse` (`Warehouse_ID`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`Category_ID`) REFERENCES `category` (`Category_ID`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`Warehouse_ID`) REFERENCES `warehouse` (`Warehouse_ID`);
+
+
